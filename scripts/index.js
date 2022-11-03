fa5ea471ab7d92ba6a785d0bbe4ad6df34c4d3c4
Megan Sampson <msampson@nyc.yearup.org>
2022-11-03 14:38:09 -0400

    completed football excercise

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,58 @@
 "use strict";
+
 window.onload = init;
 
-   function init() {
-    const selectbtn = document.getElementById("selectbtn");
-    selectbtn.onclick =selectbtnOnClick;
-    // other events handlers connected here
-   }
-
-
-let teams = [
+const teams = [
  {code:"DAL", name:"Dallas Cowboys", plays:"Arlington, TX"},
  {code:"DEN", name:"Denver Broncos", plays:"Denver, CO"},
  {code:"HOU", name:"Houston Texans", plays:"Houston, TX"},
- {code:"KAN", name:"Kansas City Chiefs", 
- plays:"Kansas City, MO"},
+ {code:"KAN", name:"Kansas City Chiefs",  plays:"Kansas City, MO"},
 ];
 
-   
+
+function init() {
+ fillDropdown();
+ const selectbtn = document.getElementById("selectbtn");
+ selectbtn.onclick = selectbtnOnClick;
+ // other events handlers connected here
+}
+  
+function fillDropdown(){
+
+    const footballTeamsList = document.getElementById("footballTeamsList");
+    let selectTeamOption = document.createElement("option");
+    selectTeamOption.value = "";
+    selectTeamOption.textContent = "Select a Team ..";
+    footballTeamsList.appendChild(selectTeamOption);
+    
+    
+    
+    let teamsLength = teams.length;
+    for(let i = 0 ; i < teamsLength; i++){
+        let newOption = document.createElement("option");
+        newOption.value = teams[i].code;
+        newOption.textContent = teams[i].name;
+
+        footballTeamsList.appendChild(newOption);
+    }
+}
+
    function selectbtnOnClick(){
     const footballTeamsList = document.getElementById("footballTeamsList");
-     
-    let length = teams.length;
-    for(let i = 0; i < length; i++) {
+     let selectedTeamCode = footballTeamsList.value;
+    let selectedTeam = getTeamFromCode(teams, selectedTeamCode);
 
-        let theOption = new Option(teams[i]);
-        footballTeamsList.appendChild(theOption) ;
+    let message = `You selected the ${selectedTeam.name} (${selectedTeam.code}) who play in ${selectedTeam.plays}`;
+    const teamInformation = document.getElementById("teamInformation");
+    teamInformation.innerHTML = message;
+
+   }
+   function getTeamFromCode(teams, code){
+    let teamscount = teams.length;
+    for(let i = 0; i < teamscount;  i++){
+        if(teams[i].code == code){
+          return teams[i];
+        }
     }
+    return null;
    }
